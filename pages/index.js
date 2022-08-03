028b8f8ba4bba3ab30c1ec96afff8668cb8b8491
김재민 <jay@gimjaemin-ui-MacBookPro.local>
2022-08-03 21:39:13 +0900

    finished

--- pages/index.js
+++ pages/index.js
@@ -1,56 +1,52 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [innerTimers, setInnerTimers] = useState([])
-  const [timer, setTimer] = useState(0)
-  const [timers, setTimers] = useState([])
-
-  const onChangeTimer = (e) => {
-    setTimer(e.target.value)
-  }
+  const [timers, setTimers] = useState([]);
 
   const onKeyUpTimer = (e) => {
-    if(e.key !== "Enter") return
+    const timer = e.target.value;
+    if (timer > 60) e.target.value = 60;
+    if (timer < 1) e.target.value = 1;
+    if (e.key !== "Enter") return;
 
-    if ( timer > 60 || timer < 1){
-      alert("시간은 1~60 사이")
-      return
+    if (timer > 60 || timer < 1) {
+      alert("시간은 1~60 사이");
+      return;
     }
 
-    if(e.key === "Enter"){
-      let count = timer
-      setTimers((prev) => [...prev, count])
-      setInnerTimers((prev) => [...prev, count])
-      
-      const individualTimer = setInterval(()=>{
-        let index = timers.length
-        setInnerTimers((prev) => {
-          prev[index] = prev[index] - 1
-          return prev
-        })
-        
-        console.log(count, innerTimers)
-        count === 0 && clearInterval(individualTimer)
+    if (e.key === "Enter") {
+      let count = timer;
+      setTimers((prev) => [...prev, count]);
 
-        count--
-      },1000)
+      const individualTimer = setInterval(() => {
+        let index = timers.length;
+        setTimers((prev) => {
+          prev[index] = count;
+
+          return [...prev];
+        });
+
+        count--;
+        count === 0 && clearInterval(individualTimer);
+      }, 1000);
     }
-  }
+  };
 
   const onClickClear = () => {
-    setTimers([])
-  }
+    setTimers([]);
+  };
 
   return (
     <div>
-      <input type="number" min={1} max={60} onChange={onChangeTimer} onKeyUp={onKeyUpTimer}/>
+      <input type="number" min={1} max={60} onKeyUp={onKeyUpTimer} />
       <ul>
-        {innerTimers.map((el,idx) => (
-        <li key={idx}>timer{idx}:{el}</li>
-      ))}
+        {timers.map((el, idx) => (
+          <li key={idx}>
+            timer{idx}:{el}
+          </li>
+        ))}
       </ul>
       <button onClick={onClickClear}>모두 종료</button>
     </div>
-    
   );
 }
